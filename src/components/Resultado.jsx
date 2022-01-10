cdb5092f7db9880b1d69ca8442e6e3b97bd47006
Diego Aldao <diego_ind@live.com.ar>
2022-01-10 10:33:55 -0300

    primer commit

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled";
+
+const Respuesta = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: start;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+const Imagen = styled.img`
+  display: block;
+  width: 150px;
+`;
+const Texto = styled.p`
+  font-size: 1.125rem;
+  span {
+    font-weight: 700;
+  }
+`;
+const Precio = styled.p`
+  font-size: 1.5rem;
+  span {
+    font-weight: 700;
+  }
+`;
+
+function Resultado({ resultado }) {
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
+    resultado;
+  return (
+    <Respuesta>
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen criptomoneda"
+      />
+      <div>
+        <Precio>
+          El precio es de: <span>{PRICE}</span>
+        </Precio>
+        <Texto>
+          Precio mas alto del dia: <span>{HIGHDAY}</span>
+        </Texto>
+        <Texto>
+          Precio mas bajo del dia: <span>{LOWDAY}</span>
+        </Texto>
+        <Texto>
+          Variacion ultimas 24 horas: <span>{CHANGEPCT24HOUR}%</span>
+        </Texto>
+        <Texto>
+          Ultima actualizacion <span>{LASTUPDATE}</span>
+        </Texto>
+      </div>
+    </Respuesta>
+  );
+}
+
+export default Resultado;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,6 @@
+import "../styles/Spinner.css";
+function Spinner() {
+  return <div className="spinner"></div>;
+}
+
+export default Spinner;
